dc4cbd0fb40a156e503acfefb413e93767840715
tomaszszd <tomaszszd@gmail.com>
2022-01-12 14:55:09 +0100

    Fade in effect

--- dist/js/observers.js
+++ dist/js/observers.js
@@ -1,8 +1,10 @@
 const header = document.querySelector(".container_navigation");
 const sectionOne = document.querySelector("#home");
 
-const sectionOptions = {};
+const faders = document.querySelector(".fade-in");
 
+// section observer for NAVIGATION SCROLL EFFECT
+const sectionOptions = {};
 const sectionObserver = new IntersectionObserver(function (
   entries,
   sectionObserver
@@ -22,3 +24,23 @@ const sectionObserver = new IntersectionObserver(function (
 sectionOptions);
 
 sectionObserver.observe(sectionOne);
+
+// fade-in observer for FADE-IN EFFECT
+const fadeinOptions = {
+  threshold: 1,
+  rootMargin: "0px 0px -100px 0px"
+};
+const fadeinObserver = new IntersectionObserver(function(entries, fadeinObserver) {
+  entries.forEach(entry => {
+    if(!entry.isIntersecting) {
+      return; 
+    } else {
+      entry.target.classList.add("appear");
+      fadeinObserver.unobserve(entry.target);
+    }
+  });
+}, fadeinOptions);
+
+faders.forEach(fader => {
+  fadeinObserver.observe(fader);
+});
